5b71a3b577292ad8bb40fed4ddefe9d96d33151d
tjwjdvkf <tjwjdvkf2@naver.com>
2018-01-16 22:02:22 +0900

    일단 패키지만 나눠놨어 / index는 따로 빼놔서 index.do 하면 일단 나올거야 / 폴더 하나 더 만들어서 인클루드 빼서 하면됨

---
+++ src/main/java/com/kjh/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.kjh.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/index.do")
+	public String index() {
+		return "index";
+	}
+}
