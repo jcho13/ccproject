2d1f0bbc5ec3f855dbffba88a9a419efe339ed92
daphness88 <rahart.1024@gmail.com>
2018-01-21 18:12:19 +0900

    20180121_설정파일 변경

--- src/main/java/com/kjh/user/serviceimpl/TestServiceImpl1.java
+++ src/main/java/com/kjh/user/serviceimpl/TestServiceImpl1.java
@@ -1,15 +1,6 @@
 package com.kjh.user.serviceimpl;
 
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.springframework.stereotype.Service;
-
 import com.kjh.user.service.TestService1;
-import com.sjh.admin.dao.AdminUserServiceDAO;
-import com.sjh.admin.service.AdminUserService;
-import com.sjh.admin.vo.BoardVO;
 
 //@Service("service")
 public class TestServiceImpl1 implements TestService1 {
